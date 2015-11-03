b3a295d8a26880a57ac468b9d37d3acd7d6d4041
Eleni Skouroupathi <eleni59@hotmail.com>
2015-11-03 17:53:16 +0000

    Chapter 11

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+feature 'Creates links' do
+	scenario 'Stores website links in bookmark' do
+	visit '/links/new'
+	fill_in :url, with: 'http://www.google.com'
+	fill_in :name, with: 'Google'
+	click_button 'Create link'
+
+	expect(current_path).to eq '/links'
+
+	within 'ul#links' do
+    expect(page).to have_content('Google')
+end
+end
+end
